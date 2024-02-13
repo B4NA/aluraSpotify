c9fa016439777ad0fd5641f5d29f6ae9f553026b
BANAAAAAAAAAAAAAAAAAA <142839926+B4NA@users.noreply.github.com>
2024-02-13 15:36:20 -0300

    Update Main.js

--- Projeto-React/spotify-react/src/Main/Main.js
+++ Projeto-React/spotify-react/src/Main/Main.js
@@ -25,37 +25,34 @@ const Main = () => {
         const resultSearch = document.getElementById('result-search');
         const resultMain = document.getElementById('result-main');
 
-        function displayResults(result) {
+        function displayResults(result, searchTerm) {
             resultMain.classList.add('hidden');
-
-            let index = 1;
+            
             let artistGrid = document.querySelector('.grid-container');
+            let lang = document.querySelector('.languages__select').selectedIndex;
+            
             artistGrid.innerHTML = '';
-
+            
             result.forEach(element => {
-                artistGrid.innerHTML += `<div class="artist-card" id="${index}"><div class="card-img"><img id="artist-img${index}" class="artist-img" /><div class="play"><span class="fa fa-solid fa-play"></span></div></div><div class="card-text"><a title="Foo Fighters" class="vst" href=""></a><span class="artist-name" id="artist-name${index}"></span><span class="artist-categorie">Artista</span></a></div></div>`
-                let artistName = document.getElementById(`artist-name${index}`);
-                let artistImage = document.getElementById(`artist-img${index}`);
-                artistName.innerText = element.name;
-                artistImage.src = element.urlImg;
-                index += 1;
+                if (element.name.toLowerCase().includes(searchTerm) === true) {
+                  artistGrid.innerHTML += `<div class="artist-card"><div class="card-img"><img src="${element.urlImg}" class="artist-img" /><div class="play"><span class="fa fa-solid fa-play"></span></div></div><div class="card-text"><a title="${element.name}" class="vst" href=""></a><span class="artist-name">${element.name}</span><span class="artist-categorie">${artistLang[lang]} - ${element.genre}</span></a></div></div>`
+                };
             });
-
+            
             resultSearch.classList.remove('hidden');
         };
-
+            
         function requestApi(searchTerm) {
             // O parâmetro name_like da URL abaixo pode não funcionar em algumas versões
             const url = `http://localhost:3000/artists`;
             fetch(url)
-                .then((response) => response.json())
-                .then((result) => displayResults(result))
-                .catch((error) => console.error('Erro: ', error))
-        };
-
+            .then((response) => response.json())
+            .then((result) => displayResults(result, searchTerm))
+            };
+            
         document.addEventListener('input', function() {
             const searchTerm = searchInput.value.toLowerCase();
-
+            
             if (searchTerm === '') {
                 resultSearch.classList.add('hidden');
                 resultMain.classList.remove('hidden');
